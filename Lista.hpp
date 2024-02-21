60695219cd03ade7e33796cb9bf29c190603ae46
JosueDPA <90888468+JosueDPA@users.noreply.github.com>
2024-02-20 23:40:39 -0600

    organizacion
    se elimino una carpeta

---
+++ Lista.hpp
@@ -0,0 +1,19 @@
+#include"Nodo.hpp"
+
+
+#ifndef LISTA
+#define LISTA
+
+class Lista{
+	public:
+		Lista();
+		Lista(int );
+		void pushFront(int );
+		void pushBack(int );
+		void imprimir();
+		int popFront();
+		int popBack();
+	private:
+		Nodo *head;
+};
+#endif
---
+++ Nodo.cpp
@@ -0,0 +1,7 @@
+#include"Nodo.hpp"
+
+
+Nodo::Nodo(int key){
+	this->key=key;
+	this->next=0;
+}
